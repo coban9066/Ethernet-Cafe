f4786ba1d81e1ad7b808c14f8a854cd3638db6a0
Mehmet Çoban <137454414+coban9066@users.noreply.github.com>
2025-04-16 00:23:29 +0300

    v2

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,51 @@
-const images = [
+const shopImages = [
     './img/ic-mekan.jpeg',
     './img/ic-mekan2.jpeg',
     './img/ic-mekan3.jpeg',
-    './img/ic-mekan4.jpeg'
+    './img/ic-mekan4.jpeg',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg'
 ];
 
-let currentIndex = 0;
-const imageElement = document.getElementById('gallery-image');
+const equipmentImages = [
+    './img/7.png',
+    './img/8.png',
+    './img/9.jpg',
+    './img/10.jpg',
+    './img/11.jpg',
+    './img/12.jpg'
 
+
+
+    
+
+
+
+    
+];
+
+let showingEquipment = false;
+let currentIndex = 0;
+let currentImages = shopImages;
+const imageElement = document.getElementById('gallery-image');
+const toggleLink = document.getElementById('toggle-gallery');
+
+// Slayt değişimi
 setInterval(() => {
-    currentIndex = (currentIndex + 1) % images.length;
-    imageElement.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % currentImages.length;
+    imageElement.src = currentImages[currentIndex];
 }, 7000);
+
+// Tıklamayla resim listesini değiştirme
+toggleLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    showingEquipment = !showingEquipment;
+    currentImages = showingEquipment ? equipmentImages : shopImages;
+    toggleLink.textContent = showingEquipment ? "Dükkan Detaylarını Görüntüle" : "Ekipmanları Görüntüle";
+    currentIndex = 0;
+    imageElement.src = currentImages[currentIndex];
+});
